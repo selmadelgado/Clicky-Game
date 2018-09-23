324c9961b8c8ef9b4a49c855f63eb0992d2d957c
Selma Delgado <selmadelgado@gmail.com>
2018-09-23 18:12:21 -0500

    Added images JSON

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component} from 'react';
 import './App.css';
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import CardContainer from "./components/CardContainer"
 
 class App extends Component {
+  // setting the state
+  state = {
+    count: 0,
+    topScore: 0
+  }
+
+  // update the current score count in the nav
+  updateCurrentScore = (newCount) => {
+    // set the new count as the count
+    this.setState({count: newCount});
+  }
+
+  // update the top score in the nav
+  updateTopScore = (newTop) => {
+    // if the new top score is higher than the current
+    if (newTop > this.state.topScore) {
+      // then set the state but you have to subtract 1
+      this.setState({topScore: newTop - 1})
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Navbar score={this.state.count} top={this.state.topScore}/>
+        <Header/>
+        <CardContainer updateCurrentScore={this.updateCurrentScore} updateTopScore={this.updateTopScore}/>
       </div>
     );
   }
